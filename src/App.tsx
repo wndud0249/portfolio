a7c71e431cf4744c8b142bbc15d198c7d550ac5d
jjyfm <jjyfm@promotion.co.kr>
2022-11-07 18:14:28 +0900

    developing typing effect

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import cssImg from './css/images/css3.png';
 import sassImg from './css/images/sass.png';
 import bootStrapImg from './css/images/bootstrap.jpg';
 import jsImg from './css/images/js.png';
-import reactImg from './css/images/react.jpeg';
+import reactImg from './css/images/react.png';
 import reduxImg from './css/images/redux.png';
 import reduxToolKitImg from './css/images/toolkit.png';
 import reduxSagaImg from './css/images/redux_saga.png';
@@ -15,9 +15,14 @@ import nextJsImg from './css/images/nextjs.svg';
 import gitImg from './css/images/git.png';
 import gitHubImg from './css/images/github-sign.png';
 import gitLabImg from './css/images/gitlab.png';
+import slackImg from './css/images/slack.png';
+import confluenceImg from './css/images/confluence.png';
+import jiraImg from './css/images/jira.png';
+import zeplinImg from './css/images/zeplin.png';
 
 function App() {
-  const [focusedStage, setFocusedStage] = useState('stage_1-1');
+  const [focusedStage, setFocusedStage] = useState('');
+  const [letterTobeDel, setLetterTobeDel] = useState<string[]>([]);
   const stageData = useRef(focusedStage);
 
   function functionToCall(e: any) {
@@ -84,6 +89,7 @@ function App() {
   }, []);
 
   useEffect(() => {
+    if (!focusedStage) return;
     stageData.current = focusedStage;
 
     const widthPoint = stageData.current.split('_')[1].split('-')[1];
@@ -100,6 +106,7 @@ function App() {
   }, [focusedStage]);
 
   useEffect(() => {
+    setFocusedStage('stage_1-1');
     const introPage = document.querySelector('.stage_1-1');
     introPage?.scrollIntoView();
   }, []);
@@ -109,6 +116,72 @@ function App() {
   };
 
   console.log(focusedStage);
+  /* 타이핑 */
+
+  let typingTimeOut: any;
+
+  const speed = 100;
+  // 타이핑 효과
+  const typing = async () => {
+    if (!focusedStage) return;
+    console.log('들어옴');
+    console.log(focusedStage);
+    const $text = document.querySelector('.typing');
+    const letterObjs = [
+      { stageName: 'stage_1-1', text: 'hello' },
+      { stageName: 'stage_2-1', text: 'Develop' },
+      { stageName: 'stage_2-2', text: 'Others' },
+    ];
+    if (typingTimeOut) clearTimeout(typingTimeOut);
+    if ($text instanceof HTMLElement) {
+      // const letter = letters[i].split('');
+      const letter = letterObjs.filter((obj) => obj.stageName === focusedStage)[0].text.split('');
+
+      setLetterTobeDel(letter);
+      while (letter.length) {
+        await wait(speed);
+        $text.innerHTML += letter.shift();
+      }
+      // // 잠시 대기
+      // await wait(800);
+      // // 지우는 효과
+      // remove();
+    }
+  };
+
+  // 글자 지우는 효과
+  const remove = async () => {
+    const $text = document.querySelector('.typing');
+    if ($text instanceof HTMLElement) {
+      const letter = letterTobeDel;
+      while (letter.length) {
+        await wait(speed);
+        letter.pop();
+        $text.innerHTML = letter.join('');
+      }
+      console.log(letterTobeDel);
+      // 다음 순서의 글자로 지정, 타이핑 함수 다시 실행
+      // i = !letterObjs[i + 1] ? 0 : i + 1;
+      typing();
+    }
+  };
+
+  // 딜레이 기능 ( 마이크로초 )
+  function wait(ms: number) {
+    return new Promise((res) => setTimeout(res, ms));
+  }
+
+  useEffect(() => {
+    if (!focusedStage) return;
+    if (typingTimeOut) clearTimeout(typingTimeOut);
+
+    // 초기 실행
+    remove();
+    typingTimeOut = setTimeout(typing, 1500);
+    return () => {
+      clearTimeout(typingTimeOut);
+    };
+  }, [focusedStage]);
 
   return (
     <div className="App">
@@ -136,41 +209,42 @@ function App() {
             </div>
           </div>
         </div>
+        <div className="typing">FrontEnd Developer, ju young's portfolio</div>
+
         <div className="contents">
           <div className="content_1">
             <div className="stage_1-1">
               <div className="content_wrapper">
-                <div className="title_typing">I'm a FrontEnd developer, ju young !</div>
-                <div className="title_about">
+                <article className="title_about">
                   <div className="about_first">
                     <div className="name">
-                      <div className="name_icon"></div>
-                      <div className="name_content"></div>
+                      <div className="name_icon">이름</div>
+                      <div className="name_content">ㅁㄴㅇㄹ</div>
                     </div>
                     <div className="birth">
-                      <div className="birth_icon"></div>
-                      <div className="birth_content"></div>
+                      <div className="birth_icon">birth</div>
+                      <div className="birth_content">2022-22-22</div>
                     </div>
                     <div className="addr">
-                      <div className="addr_icon"></div>
-                      <div className="addr_content"></div>
+                      <div className="addr_icon">address</div>
+                      <div className="addr_content">seoul</div>
                     </div>
                   </div>
                   <div className="about_second">
                     <div className="phone">
-                      <div className="phone_icon"></div>
-                      <div className="phone_content"></div>
+                      <div className="phone_icon">mobile</div>
+                      <div className="phone_content">010-2345-4104</div>
                     </div>
                     <div className="email">
-                      <div className="email_icon"></div>
-                      <div className="email_content"></div>
+                      <div className="email_icon">E-mail</div>
+                      <div className="email_content">wndud0249@naver.com</div>
                     </div>
                     <div className="blog">
-                      <div className="blog_icon"></div>
-                      <div className="blog_content"></div>
+                      <div className="blog_icon">blog</div>
+                      <div className="blog_content">blog address</div>
                     </div>
                   </div>
-                </div>
+                </article>
               </div>
             </div>
           </div>
@@ -192,12 +266,19 @@ function App() {
                   <article className="js">
                     <div className="js_header h1 bold">Javascript</div>
                     <div className="js_images">
-                      <img src={jsImg} alt="js" />
-                      <img src={typeScriptImg} alt="typescript" />
-                      <img src={reactImg} alt="react" />
-                      <img src={reduxImg} alt="redux" />
-                      <img src={reduxSagaImg} alt="redux_saga" />
-                      <img src={reduxToolKitImg} alt="redux_tool_kit" />
+                      <div>
+                        <img src={jsImg} alt="js" />
+                        <img src={typeScriptImg} alt="typescript" />
+                      </div>
+                      <div>
+                        <img src={reactImg} alt="react" />
+                        <img src={nextJsImg} alt="next_js" />
+                      </div>
+                      <div>
+                        <img src={reduxImg} alt="redux" />
+                        <img src={reduxSagaImg} alt="redux_saga" />
+                        <img src={reduxToolKitImg} alt="redux_tool_kit" />
+                      </div>
                     </div>
                   </article>
                 </div>
@@ -210,21 +291,18 @@ function App() {
                   <article className="versionControl">
                     <div className="versionControl_header h1 bold">Version Control</div>
                     <div className="versionControl_images">
-                      <img src={htmlImg} alt="html5" />
-                      <img src={cssImg} alt="css3" />
-                      <img src={sassImg} alt="sass" />
-                      <img src={bootStrapImg} alt="bootstrap" />
+                      <img src={gitImg} alt="git" />
+                      <img src={gitHubImg} alt="git_hub" />
+                      <img src={gitLabImg} alt="git_lab" />
                     </div>
                   </article>
                   <article className="communication">
                     <div className="communication_header h1 bold">Communication</div>
                     <div className="communication_images">
-                      <img src={jsImg} alt="js" />
-                      <img src={typeScriptImg} alt="typescript" />
-                      <img src={reactImg} alt="react" />
-                      <img src={reduxImg} alt="redux" />
-                      <img src={reduxSagaImg} alt="redux_saga" />
-                      <img src={reduxToolKitImg} alt="redux_tool_kit" />
+                      <img src={slackImg} alt="slack" />
+                      <img src={jiraImg} alt="jira" />
+                      <img src={confluenceImg} alt="confluence" />
+                      <img src={zeplinImg} alt="zeplin" />
                     </div>
                   </article>
                 </div>
